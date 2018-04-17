0ee52dcef0f4baedfed7017d4cb60dea46db2250
David Adams <dadams@clearc2.com>
2018-04-17 13:04:15 -0500

    cleaning up package

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,9 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
-const extractSASS = new ExtractTextPlugin('[name].css')
 
 let config = {
   entry: {
@@ -20,8 +17,7 @@ let config = {
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       template: './src/index.ejs'
-    }),
-    extractSASS
+    })
   ],
   output: {
     filename: '[name].bundle.js',
@@ -38,56 +34,6 @@ let config = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: { loader: 'babel-loader' }
-      },
-      {
-        test: /\.css$/,
-        loader: extractSASS.extract({
-          fallback: 'style-loader',
-          use: [ 'css-loader' ]
-        })
-      },
-      {
-        test: /\.scss/,
-        loader: extractSASS.extract({
-          fallback: 'style-loader',
-          use: [ 'css-loader', 'sass-loader?sourceMap' ]
-        })
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: [ 'file-loader' ]
-      },
-      {
-        test: /\.mp3$/,
-        use: [ 'file-loader' ]
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: [{
-          loader: 'url-loader',
-          options: { limit: 10000, mimetype: 'application/font-woff' }
-        }]
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'url-loader',
-          options: { limit: 10000, mimetype: 'application/octet-stream' }
-        }]
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'url-loader',
-          options: { limit: 10000, mimetype: 'image/svg+xml' }
-        }]
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [{
-          loader: 'url-loader',
-          options: { limit: 8192 }
-        }]
       }
     ]
   }
