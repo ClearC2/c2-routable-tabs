6bde2a5ec32153cd5defea76ac3b9af8fd90c375
David Adams <dadams@clearc2.com>
2018-04-17 12:36:39 -0500

    cleaned up test helper

--- test/test-helper.js
+++ test/test-helper.js
@@ -2,33 +2,11 @@ import 'babel-polyfill'
 import chai from 'chai'
 import chaiImmutable from 'chai-immutable'
 import dirtyChai from 'dirty-chai'
-import jsdom from 'jsdom'
 import 'jsdom-global/register'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 global.NODE_ENV = 'testing'
-
-const dom = new jsdom.JSDOM('<!doctype html><html><body></body></html>')
-const {window} = dom
-
-function copyProps (src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .reduce((result, prop) => ({
-      ...result,
-      [prop]: Object.getOwnPropertyDescriptor(src, prop)
-    }), {})
-  Object.defineProperties(target, props);
-}
-
-global.window = window
-global.document = window.document
-global.navigator = {
-  userAgent: 'node.js'
-}
-copyProps(window, global)
-
 global.localStorage = mockLocalStorage()
 
 Enzyme.configure({ adapter: new Adapter() })
